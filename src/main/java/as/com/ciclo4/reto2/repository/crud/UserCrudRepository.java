5337c8c408e94666fa6d4366bba7a819477e9d68
Juan Rodriguez <79487319+JuanRodriguez84@users.noreply.github.com>
2021-12-01 22:11:39 -0500

    Primer Commit

---
+++ src/main/java/as/com/ciclo4/reto2/repository/crud/UserCrudRepository.java
@@ -0,0 +1,19 @@
+package as.com.ciclo4.reto2.repository.crud;
+
+import as.com.ciclo4.reto2.model.User;
+import java.util.Optional;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+/**
+ *
+ * @author Juan Carlos Rodríguez
+ */
+public interface UserCrudRepository extends MongoRepository<User,Integer> {
+    //aqui defino consultas adicionales que no estan predeterminadas en la Interfaz "MongoRepository"
+    
+    public Optional <User> findByEmail(String email);
+    
+    public Optional<User> findByEmailAndPassword(String email, String password);
+    
+}
+
